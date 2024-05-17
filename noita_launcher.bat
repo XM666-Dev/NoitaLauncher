0495e132046b35f741733e152fa36b383a45a6a2
ImmortalDamned <99076011+XM666-Dev@users.noreply.github.com>
2024-05-18 07:50:50 +0800

    Update noita_launcher.bat

--- noita_launcher.bat
+++ noita_launcher.bat
@@ -1,8 +1,10 @@
 setlocal
 
-rem NoitaLauncher - Version 1.1.1
+rem NoitaLauncher - Version 1.1.2
 rem Created by ImmortalDamned
 rem Github https://github.com/XM666-Dev/NoitaLauncher
+
+chcp 65001
 
 for /f "skip=1 tokens=2*" %%i in ('reg query HKEY_CURRENT_USER\Software\Valve\Steam /v SteamPath') do (
     set game_path=%%~fj\steamapps\common\Noita
@@ -11,7 +13,7 @@ set save_path=Nolla_Games_Noita
 
 set config_path=config.ini
 for /f "eol=; delims== tokens=1* usebackq" %%i in ("%config_path%") do (
-    set %%i=%%j    
+    set %%i=%%j
 )
 
 echo 2>"%config_path%"
@@ -26,11 +28,11 @@ call islinker "%USERPROFILE%\AppData\LocalLow\Nolla_Games_Noita" "%save_path%"
 
 for /d %%i in ("%game_path%\mods\*") do (
 	for /f "usebackq" %%j in ("%%i\mod_id.txt") do (
-		ren %%i %%j
+		ren "%%i" "%%j"
 	)
 )
 
 cd /d %game_path%
-start noita.exe
+start noita
 
 endlocal
